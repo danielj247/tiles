3323e8eb658db19b48a9ba6e4033c18b1569da9f
Daniel Jarrett <me@danieljarrett.dev>
2023-09-28 15:51:20 +0100

    github header link and baseUrl set in tsconfig

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,6 +1,7 @@
 import { useStore } from "@/store";
 import MapDropdown from "@/ui/components/map-dropdown";
 import { Button } from "@/ui/components/ui/button";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 export default function App() {
   const selectedMap = useStore((state) => state.map);
@@ -12,9 +13,14 @@ export default function App() {
           <Button variant="secondary">New</Button>
         </div>
         <div className="flex flex-col items-center justify-center">
-          <h1 className="text-white text-xl">tiles.</h1>
+          <a href="https://github.com/danielj247/tiles" target="_blank">
+            <h1 className="text-white text-xl flex items-center">
+              tiles.
+              <GitHubLogoIcon className="ml-2 w-5 h-5" />
+            </h1>
+          </a>
           {selectedMap && (
-            <p className="text-white">
+            <p className="text-white mt-3">
               Selected map:{" "}
               <span className="bg-pink-600 py-1 px-2 ml-1 rounded">
                 {selectedMap.name}

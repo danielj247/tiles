3f6bc699db96a279bf3f61a67d1ff5dc94c6fc86
Daniel Jarrett <me@danieljarrett.dev>
2023-09-28 19:36:06 +0100

    update typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
@@ -17,5 +16,5 @@ export default defineConfig({
     ],
   },
 
-  plugins: [tsconfigPaths(), react()],
+  plugins: [react()],
 });

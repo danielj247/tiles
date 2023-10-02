6c875ae1c04df5151a11cb81383db491d5a3ff13
Daniel Jarrett <me@danieljarrett.dev>
2023-10-03 00:32:47 +0100

    add hrefs to links and transitions

--- src/ui/components/home-screen-card.tsx
+++ src/ui/components/home-screen-card.tsx
@@ -28,16 +28,24 @@ export default function HomeScreenCard() {
           </p>
           <ul className="flex flex-col gap-y-2 mt-2">
             <li>
-              <span className="text-black">
+              <a
+                href="https://github.com/danielj247/tiles"
+                target="_blank"
+                className="text-black"
+              >
                 <GitHubLogoIcon className="inline" />{" "}
                 <span className="underline">GitHub project</span>
-              </span>
+              </a>
             </li>
             <li>
-              <span className="text-black">
+              <a
+                href="https://github.com/danielj247"
+                target="_blank"
+                className="text-black"
+              >
                 <GitHubLogoIcon className="inline" />{" "}
                 <span className="underline">@danielj247</span>
-              </span>
+              </a>
             </li>
           </ul>
         </CardDescription>
@@ -47,14 +55,14 @@ export default function HomeScreenCard() {
           <DialogTrigger asChild>
             <div
               role="button"
-              className="cursor-pointer flex items-center space-x-4 rounded-md border p-4 hover:bg-black hover:text-white group"
+              className="cursor-pointer flex items-center space-x-4 rounded-md border p-4 hover:bg-black hover:text-white group transition-colors"
             >
               <FilePlusIcon />
               <div className="flex-1 space-y-1">
                 <p className="text-sm font-medium leading-none">
                   Create a new map
                 </p>
-                <p className="text-sm text-zinc-500 group-hover:text-white">
+                <p className="text-sm text-zinc-500 group-hover:text-white transition-colors">
                   Start building your map from scratch
                 </p>
               </div>
@@ -67,12 +75,12 @@ export default function HomeScreenCard() {
         <div
           role="button"
           onClick={loadMap}
-          className="cursor-pointer flex items-center space-x-4 rounded-md border p-4 hover:bg-black hover:text-white group"
+          className="cursor-pointer flex items-center space-x-4 rounded-md border p-4 hover:bg-black hover:text-white group transition-colors"
         >
           <FileUpIcon />
           <div className="flex-1 space-y-1">
             <p className="text-sm font-medium leading-none">Load a map</p>
-            <p className="text-sm text-zinc-500 group-hover:text-white">
+            <p className="text-sm text-zinc-500 group-hover:text-white transition-colors">
               Continue working on a different masterpiece
             </p>
           </div>
